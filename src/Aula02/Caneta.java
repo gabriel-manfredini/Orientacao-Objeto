8b2f0f780ff759cb82591a1c7a52afa7754eedc8
Gabriel Manfredini <gbueno@br.ibm.com>
2017-10-26 21:47:04 -0200

    First commit at Class02, Class Aula02 and Caneta

---
+++ src/Aula02/Caneta.java
@@ -0,0 +1,24 @@
+package Aula02;
+
+public class Caneta {
+	String modelo;
+	String cor;
+	float ponta;
+	int carga;
+	boolean tampada;
+
+	void status(){
+		System.out.print("Uma caneta " + this.cor);
+	}
+	void rabiscar(){
+		
+	}
+	
+	void tampar(){
+		
+	}
+	
+	void destampar(){
+		
+	}
+}
