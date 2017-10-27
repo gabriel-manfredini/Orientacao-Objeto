0502a9dd3df93c54546d960fce6c14fa667d7597
Gabriel Manfredini <gbueno@br.ibm.com>
2017-10-26 22:15:52 -0200

    Change name of class

--- src/Aula02/Teste.java
+++ src/Aula02/Teste.java
@@ -1,15 +1,22 @@
 package Aula02;
 
-public class Aula02 {
+import java.util.Scanner;
+
+public class Teste {
 
 	public static void main(String[] args) {
 		Caneta c1 = new Caneta();
 		c1.cor = "Azul";
 		c1.ponta = 0.5f;
 		c1.tampada = false;
-		c1.status();
+//		c1.status();
 		c1.destampar();
 		c1.rabiscar();
+		
+		Caneta c2 = new Caneta();
+		c2.tampar();
+		c2.rabiscar();
+	
 		
 	}
 
